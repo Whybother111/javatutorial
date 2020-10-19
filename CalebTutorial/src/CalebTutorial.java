4d310996e216ab8255ba1e656b37f790b97197e7
Whybother111 <michaelhepner95@gmail.com>
2020-10-19 13:17:46 -0400

    initial commit

---
+++ CalebTutorial/src/CalebTutorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CalebTutorial {
+
+	public static void main(String[] args) {   //method      //comment
+
+		System.out.println("Enter your name");
+		
+		Scanner in = new Scanner(System.in);
+		//String s = in.nextLine();
+		
+		
+		
+		int i = 0;
+		
+		do {
+			System.out.println("i is: " + i);
+			i++;
+		} while(false);
+		
+		
+		in.close();
+		
+	}
+		
+}
+
+
